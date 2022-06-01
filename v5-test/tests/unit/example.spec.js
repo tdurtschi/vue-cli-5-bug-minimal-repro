4f8c89547ff590dfc9957eb6fd48bda7f225a4cc
Teagan Durtschi <tdurtschi@pivotal.io>
2022-06-01 17:41:36 -0400

    Add passing test to compare. Removing more deps that aren't needed to reproduce

--- v5-test/tests/unit/example.spec.js
+++ v5-test/tests/unit/example.spec.js
@@ -1,18 +1,43 @@
 import { mount } from '@vue/test-utils'
 
-const Counter = {
-  template: `
-    <div>
-      <input type="checkbox" @change="count++">Add up</input>
-      <p>Total clicks: {{ count }}</p>
-    </div>
-  `,
-  data() {
-    return { count: 0 }
+test('increments counter value on button click', async () => {
+  const Counter = {
+    template: `
+      <div>
+        <button @click="count++">Add up</button>
+        <p>Total clicks: {{ count }}</p>
+      </div>
+    `,
+    data() {
+      return { count: 0 }
+    }
   }
-}
+  
+  const wrapper = mount(Counter)
+  console.log(document.body.innerHTML);
+  const button = wrapper.find('button')
+  const text = wrapper.find('p')
 
-test('increments counter value on click', async () => {
+  expect(text.text()).toContain('Total clicks: 0')
+
+  await button.trigger('click')
+
+  expect(text.text()).toContain('Total clicks: 1')
+})
+
+test('increments counter value on checkmark click', async () => {
+  const Counter = {
+    template: `
+      <div>
+        <input type="checkbox" @change="count++">Add up</input>
+        <p>Total clicks: {{ count }}</p>
+      </div>
+    `,
+    data() {
+      return { count: 0 }
+    }
+  }
+  
   const wrapper = mount(Counter)
   console.log(document.body.innerHTML);
   const input = wrapper.find('input')
